131ae76d83ff71ed48a2dfc4f7c9d7ac36dded29
Jeff <anonymous@Anonymouss-MacBook-Pro.local>
2017-05-09 03:34:12 -0400

    dd script to zero or urandom a drive.

---
+++ auto_wipe_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while true; do
+    read -p "Are you Running a Mac?" yn
+    case $yn in
+        [Yy]* ) echo "You have specified your system as Mac" && diskutil list;;
+        [Nn]* ) echo "You have specified your system as Linux" && fdisk -l;;
+        * ) echo "Please answer yes or no.";;
+    esac
+    break
+done
+echo "Type the disk name, followed by [ENTER]: ex.(disk2)"
+read disk
+echo "Would you like to wipe your drive using "urandom" or "zero"? Please specify,followed by [ENTER]:"
+read wpefrm
+sudo dd if=/dev/$wpefrm of=/dev/$disk
+echo -e "Image Complete"
+read -s -r -p "Press any key to exit"
